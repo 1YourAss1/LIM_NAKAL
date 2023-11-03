147e39f6156b2aac27fc5eff935f4efd0036f8ee
Yuri Murashko <ymurashko@blogic.ru>
2023-11-03 15:31:29 +0300

    init

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame() {
+        this.setTitle("Накал DM25");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(420, 420);
+        this.setVisible(true);
+    }
+}
